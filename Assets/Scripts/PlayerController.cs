0c6bd215fe92d36fc48da3f01a0941edb36f7f77
Sunave <antti.suniala@gmail.com>
2015-07-05 20:56:53 +0300

    Add ship, add ship movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveShip();
+	}
+
+	void MoveShip () {
+		Vector3 shipPos = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
+		float movement = speed * Time.deltaTime;
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			shipPos.x -= movement;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			shipPos.x += movement;
+		}
+
+		this.transform.position = shipPos;
+	}
+}
