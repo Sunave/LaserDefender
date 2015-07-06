fb0dab9a03c229cb0cccaf05501e2eca22116933
Sunave <antti.suniala@gmail.com>
2015-07-06 18:02:17 +0300

    Add enemy projectiles, add ability for enemies to shoot

--- Assets/Entities/Enemy/EnemyBehaviour.cs
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class EnemyBehaviour : MonoBehaviour {
 
+	public GameObject projectile;
 	public float health = 150f;
+	public float projectileSpeed = 10f;
+	public float shotPerSeconds = 0.5f;
+
+	void Update () {
+		float probabilityOfFire = Time.deltaTime * shotPerSeconds;
+		if (Random.value < probabilityOfFire) {
+			FireBeam();
+		}
+	}
 
 	void OnTriggerEnter2D (Collider2D col) {
 		Projectile missile = col.gameObject.GetComponent<Projectile>();
@@ -14,5 +24,11 @@ public class EnemyBehaviour : MonoBehaviour {
 				Destroy(gameObject);
 			}
 		}
+	}
+
+	void FireBeam () {
+		Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
+		GameObject beam = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3 (0, -projectileSpeed, 0);
 	}
 }
--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 	public GameObject projectile;
 
 	public float speed = 15.0f;
+	public float health = 250f;
 	public float padding = 1f;
 	public float projectileSpeed = 5f;
 	public float firingRate = 0.2f;
@@ -27,6 +28,17 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKeyUp (KeyCode.Space)) CancelInvoke("FireBeam");
 	}
 
+	void OnTriggerEnter2D (Collider2D col) {
+		Projectile missile = col.gameObject.GetComponent<Projectile>();
+		if (missile) {
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
 	void MoveShip () {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.position += Vector3.left * speed * Time.deltaTime;
@@ -40,7 +52,8 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FireBeam () {
-		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		Vector3 startPosition = transform.position + new Vector3(0, 1, 0);
+		GameObject beam = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
 		beam.GetComponent<Rigidbody2D>().velocity = new Vector3 (0, projectileSpeed, 0);
 	}
 
