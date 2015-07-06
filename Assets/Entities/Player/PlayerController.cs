2fd8f22d1232ecb0849c8b5d02692e010d683a74
Sunave <antti.suniala@gmail.com>
2015-07-06 16:29:20 +0300

    Add player projectiles

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject projectile;
+
 	public float speed = 15.0f;
 	public float padding = 1f;
+	public float projectileSpeed = 5f;
+	public float firingRate = 0.2f;
 
 	float xMin;
 	float xMax;
@@ -19,6 +23,8 @@ public class PlayerController : MonoBehaviour {
 
 	void Update () {
 		MoveShip();
+		if (Input.GetKeyDown (KeyCode.Space)) InvokeRepeating("FireBeam", 0.000001f, firingRate);
+		if (Input.GetKeyUp (KeyCode.Space)) CancelInvoke("FireBeam");
 	}
 
 	void MoveShip () {
@@ -31,6 +37,11 @@ public class PlayerController : MonoBehaviour {
 		// Restrict the player to the game space
 		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
 		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+
+	void FireBeam () {
+		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3 (0, projectileSpeed, 0);
 	}
 
 }
